6d8e9abbb10f9fdd5a6cf23aa5d98f2a3bb2f31d
Juanjo Bazán <jjbazan@gmail.com>
2017-06-27 14:13:13 +0200

    removes uses of render :text

--- app/controllers/polls/nvotes_controller.rb
+++ app/controllers/polls/nvotes_controller.rb
@@ -11,13 +11,13 @@ class Polls::NvotesController < ApplicationController
     poll = Poll.find(params[:poll_id])
     nvote = current_user.get_or_create_nvote(poll)
     message = nvote.generate_message
-    render content_type: 'text/plain', status: :ok, text: "#{Poll::Nvote.generate_hash message}/#{message}"
+    render plain: "#{Poll::Nvote.generate_hash message}/#{message}", status: :ok
   end
 
   def success
     authorization_hash = request.headers["Authorization"]
     status = Poll::Nvote.store_voter(authorization_hash) ? 200 : 400
-    render content_type: 'text/plain', status: status, text: ""
+    render plain: '', status: status
   end
 
 end
